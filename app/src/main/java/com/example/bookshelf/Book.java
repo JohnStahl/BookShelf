d92a6223be76d190269de0e46d68dffbd129ab3a
John Stahl <tug89585@temple.edu>
2020-04-02 18:41:45 -0400

    Added book class

---
+++ app/src/main/java/com/example/bookshelf/Book.java
@@ -0,0 +1,22 @@
+package com.example.bookshelf;
+
+public class Book {
+
+    private int id;
+    private String title, author, coverURL;
+
+    public Book(int id, String title, String author, String coverURL){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.coverURL = coverURL;
+    }
+
+    public int getId(){ return id;}
+
+    public String getTitle(){ return title;}
+
+    public String getAuthor(){ return author;}
+
+    public String getCoverURL(){ return coverURL;}
+}
